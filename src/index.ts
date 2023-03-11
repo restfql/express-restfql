b0024c3b7b9843da8a488f5d8087438b1944cca6
kanekotic <me@kanekotic.com>
2023-03-11 22:01:38 +0100

    ALVARO |first release

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+
+import { restfql } from "./restfql";
+export default restfql;
+Object.assign(module.exports, restfql);
---
+++ src/restfql.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import gql from 'graphql-tag';
+
+const filterResponse = (response: any, model: any, acc: any) =>{
+    for (const field of model.selectionSet.selections) {
+        acc[field.name.value] = field.selectionSet? filterResponse(response[field.name.value], field, {}) : response[field.name.value]
+    }
+    return acc
+}
+export const restfql: (req: Request, res: Response, next: any) => void = (req: Request, res: Response, next: any) => {
+    let originalJson = res.json;
+    res.json = (args: any) => {
+        const query = gql`${req.query.model}`
+        return originalJson.apply(res,[filterResponse(args,query.definitions[0], {})]);
+    };
+    next();
+};
